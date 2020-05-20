37715312df4f6ffca8de5d15cbbb0b436b739ea6
n4dz <nadzamini@gmail.com>
2020-05-20 00:03:36 -0400

    exercise one done

--- workshop/0-arrays-objects/exercise-1.js
+++ workshop/0-arrays-objects/exercise-1.js
@@ -4,16 +4,26 @@
 // source: https://www.insider.com/most-popular-foods-grubhub-2018-12
 
 let mostPopularFood = [
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
   undefined,
 ];
+
+mostPopularFood.splice(0, 0, "bean burritos"); // add to index 0
+delete mostPopularFood[mostPopularFood.length - 1]; // delete last index of array
+mostPopularFood.splice(
+  mostPopularFood.length,
+  0,
+  "buffalo-flavored cauliflower"
+); // add to last index
+
+mostPopularFood.forEach((element) => console.log(element)); // output each element of array
 
 // The array holds a top-ten list of foods as ranked by Grubhub.
 // #1 and #10 are missing

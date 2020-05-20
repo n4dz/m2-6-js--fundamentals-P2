e62aceccb60441d7a98dfdd80eb80733db28f3e5
n4dz <nadzamini@gmail.com>
2020-05-20 00:10:38 -0400

    exercise 2 done

--- workshop/0-arrays-objects/exercise-2.js
+++ workshop/0-arrays-objects/exercise-2.js
@@ -1,7 +1,7 @@
 // Exercise 2
 // -------------------
-const lateComers = ['Felicia', 'Charles', 'Terry'];
-const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+const lateComers = ["Felicia", "Charles", "Terry"];
+const earlyBirds = ["Ellen", "Jerry", "Jasmine"];
 
 // You have two arrays above of people who arrived late to your party and
 // people who arrived early.
@@ -11,3 +11,8 @@ const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
 // Output them in a tidy list from first to last arrival the console.
 
 // NO for loops!
+let attendees = earlyBirds.concat(lateComers);
+
+attendees.forEach((element) => {
+  console.log(element);
+});
